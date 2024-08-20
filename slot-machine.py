938171841bbef740f9b41d2a6a6a003155ec89e5
syed <syedmosaib231@gmail.com>
2024-08-20 19:07:00 +0530

    slot machine

--- slot-machine.py
+++ slot-machine.py
@@ -1,5 +1,7 @@
+MAX_LINES = 3
 
 def deposit():
+    lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")")
     while True:
         amount = input("What would you like to deposit? $")
         if amount.isdigit():
@@ -11,3 +13,10 @@ def deposit():
         else:
             print("Please enter a number.")
     return amount
+deposit()
+
+def get_number_of_lines():
+
+
+def main():
+    balance = deposit()
